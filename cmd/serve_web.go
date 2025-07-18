5c6f5836e3bd79454c5c1dcc6842b6f5bf2ebf5c
Pardhasardhiraob29 <pardhasardhiraob@gmail.com>
2025-07-18 13:21:31 +0000

    feat: Add serve-web command

---
+++ cmd/serve_web.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var serveWebCmd = &cobra.Command{
+	Use:   "serve-web",
+	Short: "Serve the MultiClustX web UI",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Starting MultiClustX web UI...")
+		command := exec.Command("go", "run", "main_web.go")
+		command.Stdout = os.Stdout
+		command.Stderr = os.Stderr
+		err := command.Run()
+		if err != nil {
+			log.Fatalf("Error starting web server: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveWebCmd)
+}
