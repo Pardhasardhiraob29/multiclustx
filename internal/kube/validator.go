e6b0642ba8566d946de62f02a3feded60e607912
Pardhasardhiraob29 <pardhasardhiraob@gmail.com>
2025-07-19 17:44:18 +0000

    refactor: Modify PingTest and GetServerVersion to accept discovery.DiscoveryInterface

--- internal/kube/validator.go
+++ internal/kube/validator.go
@@ -3,14 +3,13 @@ package kube
 import (
 	"fmt"
 
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/discovery"
 )
 
 // PingTest checks the reachability of a Kubernetes cluster's API server.
-func PingTest(clientset kubernetes.Interface) error {
+func PingTest(discoveryClient discovery.DiscoveryInterface) error {
 	// Attempt to create a clientset to check reachability
-	_, err := clientset.Discovery().ServerVersion()
+	_, err := discoveryClient.ServerVersion()
 	if err != nil {
 		return fmt.Errorf("failed to connect to API server: %w", err)
 	}
@@ -19,8 +18,8 @@ func PingTest(clientset kubernetes.Interface) error {
 }
 
 // GetServerVersion retrieves the Kubernetes API server version.
-func GetServerVersion(clientset kubernetes.Interface) (string, error) {
-	serverVersion, err := clientset.Discovery().ServerVersion()
+func GetServerVersion(discoveryClient discovery.DiscoveryInterface) (string, error) {
+	serverVersion, err := discoveryClient.ServerVersion()
 	if err != nil {
 		return "", fmt.Errorf("error getting server version: %w", err)
 	}
